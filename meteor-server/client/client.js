bb6f5f9f390164fec59c48d797176a66c309e0f9
landart <correo@jorgealbaladejo.com>
2013-05-14 16:38:13 +0200

    added simple example of leaderboard

--- meteor-server/client/client.js
+++ meteor-server/client/client.js
@@ -1,12 +1,26 @@
-Template.hello.greeting = function () {
-  return "Welcome to hive.";
-};
+Players = new Meteor.Collection("players");
 
-Template.hello.events({
-   'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined') {
-         console.log("You pressed the button");
-      }
-   }
-});
+Template.leaderboard.players = function () {
+    return Players.find({}, {sort: {score: -1, name: 1}});
+  };
+
+  Template.leaderboard.selected_name = function () {
+    var player = Players.findOne(Session.get("selected_player"));
+    return player && player.name;
+  };
+
+  Template.player.selected = function () {
+    return Session.equals("selected_player", this._id) ? "selected" : '';
+  };
+
+  Template.leaderboard.events({
+    'click input.inc': function () {
+      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    }
+  });
+
+  Template.player.events({
+    'click': function () {
+      Session.set("selected_player", this._id);
+    }
+  }); 
