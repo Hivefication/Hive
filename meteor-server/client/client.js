aaf03908ad1f28c65c4875c95bbf1e7b081e6bb0
Nicolas Jakob <nicow.jakob@gmail.com>
2013-05-20 19:08:21 +0200

    First change of UI with Twitter Bootstrap

--- meteor-server/client/client.js
+++ meteor-server/client/client.js
@@ -1,40 +1,51 @@
-  Template.leaderboard.players = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
-  };
 
-  Template.leaderboard.selected_name = function () {
-    var player = Players.findOne(Session.get("selected_player"));
-    return player && player.name;
-  };
+Template.playerboard.players = function () {
+  return Players.find({}, {sort: {score: -1, name: 1}});
+};
 
-  Template.player.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
-  };
 
-  Template.leaderboard.events({
-    'click input.inc': function () {
-      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
-    }
-  });
-
-  Template.player.events({
-    'click': function () {
-      Session.set("selected_player", this._id);
-    }
-  }); 
-
-  Template.player_details.id = function(){
-    return Session.get("selected_player")
+Template.player.events({
+  'click': function () {
+    Session.set('selected_player', this._id);
   }
+}); 
 
-  Template.player_details.badges = function(){
-    return Players.findOne({_id:Session.get("selected_player")}).badges
+Template.player.selected = function () {
+  return Session.equals('selected_player', this._id) ? 'active' : '';
+};
+
+
+Template.player_details.events({
+  'click .inc': function () {
+    Players.update(Session.get("selected_player"), { $inc: {score: 5} });
   }
+});
 
-  Template.player_details.score = function(){
-    return Players.findOne({_id:Session.get("selected_player")}).score
-  }
+Template.player_details.selected_name = function () {
+  var player = Players.findOne(Session.get("selected_player"));
+  return player && player.name;
+};
 
+Template.player_details.num_badges = function () {
+  return Players.findOne({_id:Session.get('selected_player')}).badges.length;
+}
+
+Template.player_details.badges = function () {
+  return Players.findOne({_id:Session.get('selected_player')}).badges;
+}
+
+Template.player_details.score = function () {
+  return Players.findOne({_id:Session.get('selected_player')}).score;
+}
+
+
+
+
+
+
+  
+
+  
   Template.player_details.eventlist = function(){
     var events = Players.findOne({_id:Session.get("selected_player")}).events;
 
