9fca37295856153c72b241232c56f38fabb5cb7e
Nicolas Jakob <nicow.jakob@gmail.com>
2013-05-21 20:46:24 +0200

    Default players added on first server start

--- meteor-server/server/server.js
+++ meteor-server/server/server.js
@@ -1,3 +1,19 @@
 Meteor.startup(function () {
-   // code to run on server at startup
+  if (Players.find().count() === 0) {
+    var names = ["Ada Lovelace",
+                 "Grace Hopper",
+                 "Marie Curie",
+                 "Carl Friedrich Gauss",
+                 "Nikola Tesla",
+                 "Claude Shannon"];
+    for (var i = 0; i < names.length; i++) {
+      Players.insert({
+        name: names[i],
+        score: 0,
+        events: [],
+        badges: [],
+        rewards: []
+      });
+    }
+  }
 });
